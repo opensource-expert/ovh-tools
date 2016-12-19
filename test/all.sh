273fc46d2cce6e53c68d4dec9e7fe14691a70f95
Sylvain Viart <sylvain303@github>
2016-12-19 07:35:05 +0100

    capitalize PROJECT_ID in all.sh examples

--- test/all.sh
+++ test/all.sh
@@ -1,58 +1,63 @@
 #!/bin/bash
+#
+# some examples of usage
+#
 # ovh api wrapper
-# internal call with eval \$project_id (sourced from cloud.conf)
-# $project_id is escaped because evaled bu $instance_id is pasted on shell via tmux
+# internal call with eval \$PROJECT_ID (sourced from cloud.conf)
+# $PROJECT_ID is escaped because evaled but $instance_id is pasted on
+# shell
+# all line can be sent via tmux
 
 # get a OS image id, from cli directly
-./cloud.sh call ovh_cli --format json cloud project \$project_id image --osType linux --region GRA1 |jq '.[]|select(.name|test("Deb"))'
-./cloud.sh call ovh_cli --format json cloud project \$project_id instance create -h
+./cloud.sh call ovh_cli --format json cloud project \$PROJECT_ID image --osType linux --region GRA1 |jq '.[]|select(.name|test("Deb"))'
+./cloud.sh call ovh_cli --format json cloud project \$PROJECT_ID instance create -h
 
 # get public network id
-./cloud.sh call ovh_cli --format json cloud project \$project_id network public | jq -r '.[]|.id'
+./cloud.sh call ovh_cli --format json cloud project \$PROJECT_ID network public | jq -r '.[]|.id'
 
-./cloud.sh call ovh_cli --format json cloud project \$project_id instance unknown
+./cloud.sh call ovh_cli --format json cloud project \$PROJECT_ID instance unknown
 
 #snapshot info
-./cloud.sh call ovh_cli --format json cloud project \$project_id snapshot 1a16c40a-c61a-4412-8b3e-83127c9f3132
-./cloud.sh call ovh_cli --format json cloud project \$project_id snapshot | jq -r '.[]|.id+" "+.name+" "+.status'
+./cloud.sh call ovh_cli --format json cloud project \$PROJECT_ID snapshot 1a16c40a-c61a-4412-8b3e-83127c9f3132
+./cloud.sh call ovh_cli --format json cloud project \$PROJECT_ID snapshot | jq -r '.[]|.id+" "+.name+" "+.status'
 
 # domain ip info read awk
 instance=$(./cloud.sh status | awk '/rm.open/ { print $1}')
-read ip hostname <<< $(./cloud.sh call list_instance \$project_id $instance | awk '{ print $2,$3 }')
+read ip hostname <<< $(./cloud.sh call list_instance \$PROJECT_ID $instance | awk '{ print $2,$3 }')
 echo ip=$ip hostname=$hostname
 ./cloud.sh call set_ip_domain $ip $hostname
 
 # list instance
-./cloud.sh call ovh_cli --format json cloud project \$project_id instance
+./cloud.sh call ovh_cli --format json cloud project \$PROJECT_ID instance
 # This credential is not valid
 ./cloud.sh call ovh_cli --format json auth current-credential
 
-./cloud.sh call ovh_cli --format json cloud project \$project_id instance \
+./cloud.sh call ovh_cli --format json cloud project \$PROJECT_ID instance \
     | jq -r '.[]|.id+" "+(.ipAddresses[]|select(.type=="public")).ip+" "+.name'
 
 # with jq
 mytmp=/dev/shm/cloud_status.tmp
-./cloud.sh call get_instance_status \$project_id $instance FULL > $mytmp
+./cloud.sh call get_instance_status \$PROJECT_ID $instance FULL > $mytmp
 ip=$(jq -r '(.ipAddresses[]|select(.type=="public")).ip' < $mytmp)
 hostname=$(jq -r '.name' < $mytmp)
 ./cloud.sh call set_ip_domain $ip $hostname
 
 # project manipulation
-./cloud.sh call find_image \$project_id Deb
+./cloud.sh call find_image \$PROJECT_ID Deb
 ./cloud.sh call show_projects
-./cloud.sh call last_snapshot \$project_id
-./cloud.sh call get_flavor \$project_id
+./cloud.sh call last_snapshot \$PROJECT_ID
+./cloud.sh call get_flavor \$PROJECT_ID
 # just the id of vps-ssd-1
-./cloud.sh call get_flavor \$project_id vps-ssd-1
+./cloud.sh call get_flavor \$PROJECT_ID vps-ssd-1
 # custom instance
-./cloud.sh call create_instance \$project_id $snapshot_id $script_sshkey $hostname $init
+./cloud.sh call create_instance \$PROJECT_ID $snapshot_id $script_sshkey $hostname $init
 # works with image too, here debian 8 (from find_image)
-sshkey=$(./cloud.sh call get_sshkeys \$project_id sylvain)
-./cloud.sh call create_instance \$project_id 05045d18-6035-4dc1-9d89-259272280392 $sshkey new_name2
-./cloud.sh call create_instance \$project_id 05045d18-6035-4dc1-9d89-259272280392 $sshkey gdb.opensource-expert.com init/init_script_dhcp.sh
-./cloud.sh call create_instance \$project_id 05045d18-6035-4dc1-9d89-259272280392 $sshkey ls.opensource-expert.com init/init_root_login_OK.sh
+sshkey=$(./cloud.sh call get_sshkeys \$PROJECT_ID sylvain)
+./cloud.sh call create_instance \$PROJECT_ID 05045d18-6035-4dc1-9d89-259272280392 $sshkey new_name2
+./cloud.sh call create_instance \$PROJECT_ID 05045d18-6035-4dc1-9d89-259272280392 $sshkey gdb.opensource-expert.com init/init_script_dhcp.sh
+./cloud.sh call create_instance \$PROJECT_ID 05045d18-6035-4dc1-9d89-259272280392 $sshkey ls.opensource-expert.com init/init_root_login_OK.sh
 # manipulate instances
-./cloud.sh call list_instance \$project_id
+./cloud.sh call list_instance \$PROJECT_ID
 instance_id=some_id
 # get first instance
 instance_id=$(./cloud.sh status | awk 'NR == 1 {print $1}')
@@ -60,19 +65,19 @@ instance_id=$(./cloud.sh status | awk 'NR == 1 {print $1}')
 instance_id=$(./cloud.sh status | awk '/rm.opensource-expert.com/ {print $1}')
 echo $instance_id
 ./cloud.sh call rename_instance $instance_id NEW_NAME
-./cloud.sh call get_instance_status \$project_id
+./cloud.sh call get_instance_status \$PROJECT_ID
 # or
-./cloud.sh call get_instance_status \$project_id $instance_id
+./cloud.sh call get_instance_status \$PROJECT_ID $instance_id
 # in json
-./cloud.sh call get_instance_status \$project_id $instance_id FULL
+./cloud.sh call get_instance_status \$PROJECT_ID $instance_id FULL
 
 # test ACTIVE grep JSON
-./cloud.sh call get_instance_status \$project_id $instance_id FULL | grep '"status": "ACTIVE"'
+./cloud.sh call get_instance_status \$PROJECT_ID $instance_id FULL | grep '"status": "ACTIVE"'
 
 # ssh keys
-./cloud.sh call list_sshkeys \$project_id
-./cloud.sh call get_sshkeys \$project_id
-./cloud.sh call get_sshkeys \$project_id sylvain
+./cloud.sh call list_sshkeys \$PROJECT_ID
+./cloud.sh call get_sshkeys \$PROJECT_ID
+./cloud.sh call get_sshkeys \$PROJECT_ID sylvain
 
 # dns func
 ./cloud.sh call get_domain_record_id vim.opensource-expert.com
@@ -83,14 +88,14 @@ echo $instance_id
 # instance
 instance_id=some_id_in_list_instance
 ./cloud.sh call delete_instance $instance_id
-./cloud.sh call create_snapshot \$project_id $instance_id $snapshot_id
+./cloud.sh call create_snapshot \$PROJECT_ID $instance_id $snapshot_id
 
 # internal config
 ./cloud.sh call id_is_project A_PROJECT_ID
 ./cloud.sh call set_project A_PROJECT_ID
 ./cloud.sh call write_conf FILENAME "var=val" "var2=val" "DELETE=somevar"
 ./cloud.sh call loadconf FILENAME
-./cloud.sh call set_flavor \$project_id eg-7-flex
+./cloud.sh call set_flavor \$PROJECT_ID eg-7-flex
 cat cloud.conf
 
 # main commands
@@ -100,8 +105,8 @@ snapshot_id=$(./cloud.sh get_snap | awk '/debian-updated-base/ {print $1; exit}'
 ./cloud.sh create $snapshot_id grep2.opensource-expert.com
 
 # works with image too, here debian 8 (from find_image)
-./cloud.sh call find_image \$project_id | awk '/Debian 8/ {print $1}'
-image_id=$(./cloud.sh call find_image \$project_id | awk '/Debian 8/ {print $1}')
+./cloud.sh call find_image \$PROJECT_ID | awk '/Debian 8/ {print $1}'
+image_id=$(./cloud.sh call find_image \$PROJECT_ID | awk '/Debian 8/ {print $1}')
 
 ./cloud.sh create 05045d18-6035-4dc1-9d89-259272280392 ssh.opensource-expert.com
 ./cloud.sh create $image_id awk.opensource-expert.com init/init_root_login_OK.sh
@@ -119,8 +124,8 @@ instance_id=some_id_in_list_instance
 ./cloud.sh delete ALL
 ./cloud.sh set_all_instance_dns
 # write config file
-project_id=someproject_id_from_show_projects_or_no_arg
-./cloud.sh set_project $project_id
+PROJECT_ID=someproject_id_from_show_projects_or_no_arg
+./cloud.sh set_project $PROJECT_ID
 ./cloud.sh set_flavor vps-ssd-1
 
 # consumer_key
