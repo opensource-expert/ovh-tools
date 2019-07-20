c75cbf53c175f0fd2e62bebcde69662285728335
Sylvain Viart <sylvain303@github>
2019-07-20 11:29:58 +0200

    script our install documented in README.md

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#
+# install script
+#
+# Usage: ./install.sh
+#
+
+set -euo pipefail
+
+# empty if you run it as root
+SUDO=""
+
+# un comment to run as user
+SUDO=sudo
+
+$SUDO apt update
+$SUDO apt install -y git
+git clone https://github.com/opensource-expert/ovh-tools.git
+# install jq 1.6, not yet available in package repository
+JQ_URL=https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+$SUDO wget $JQ_URL -O /usr/local/bin/jq
+$SUDO chmod a+x /usr/local/bin/jq
+$SUDO apt install -y python-pip python-dev
+cd ~/ovh-tools
+$SUDO pip install -r requirements.txt
+
+cd ~
+git clone https://github.com/yadutaf/ovh-cli.git
+cd ovh-cli/
+$SUDO pip install wheel
+$SUDO pip install setuptools
+$SUDO pip install -r requirements.txt
+# downloads json for API, this script return 1
+./ovh-eu || true
+
+# initialize credential
+cd ~/ovh-tools
+./mk_cred.py new
+mv ovh_conf.tmp ovh.conf
+cd ../ovh-cli
+ln -s ../ovh-tools/ovh.conf .
+
+# show result
+./ovh-eu  auth current-credential
